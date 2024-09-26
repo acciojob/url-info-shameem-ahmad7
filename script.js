83be8ba047982a1dd6c3112d8fa01c7edd0b079e
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-26 21:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Get the current URL using the location object
+let currentURL = window.location.href;
+
+// Get the length of the URL
+let urlLength = currentURL.length;
+
+// Show the length using an alert
+
+alert("The length of the URL is: " + urlLength);
